af2bc0a8b95cc1eb23ad5dc00d1f6f2b7bb45576
Steve Moyer <smoyer1@selesy.com>
2019-03-20 06:51:42 -0400

    Adds error reporting to CRD loading methods

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/selesy/crd-lint/internal"
 	log "github.com/sirupsen/logrus"
 )
@@ -12,7 +14,11 @@ func main() {
 	cfg := internal.NewConfig()
 	log.Debug("Config: ", cfg)
 
-	crds := loadCRDs(cfg)
+	crds, err := loadCRDs(cfg)
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
 	if len(crds) <= 1 {
 		log.Warn("No CRDs were loaded ... no validation will be done")
 	}
@@ -26,18 +32,21 @@ func main() {
 	log.Trace("main() ->")
 }
 
-func loadCRDs(cfg internal.Config) internal.CRDMap {
+func loadCRDs(cfg internal.Config) (internal.CRDMap, error) {
 	if cfg.Offline() {
 		return loadCRDsFromPath(cfg)
 	}
 	return loadCRDsFromKubernetes(cfg)
 }
 
-func loadCRDsFromKubernetes(cfg internal.Config) internal.CRDMap {
+func loadCRDsFromKubernetes(cfg internal.Config) (internal.CRDMap, error) {
 	k8s, err := internal.NewKubernetes(cfg)
-	return make(internal.CRDMap)
+	if err != nil {
+		return nil, err
+	}
+	return k8s.CRDMap, nil
 }
 
-func loadCRDsFromPath(cfg internal.Config) internal.CRDMap {
-	return make(internal.CRDMap)
+func loadCRDsFromPath(cfg internal.Config) (internal.CRDMap, error) {
+	return make(internal.CRDMap), nil
 }
