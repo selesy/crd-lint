0f19c1ccd552a2811e2f6c1e93c7455108369c6b
Steve Moyer <smoyer@psu.edu>
2019-03-20 15:45:26 -0400

    Refactors domain specific code into internal

--- cmd/main.go
+++ cmd/main.go
@@ -14,39 +14,25 @@ func main() {
 	cfg := internal.NewConfig()
 	log.Debug("Config: ", cfg)
 
-	crds, err := loadCRDs(cfg)
+	var k8s internal.Kubernetes
+	var err error
+	if !cfg.Offline() {
+		k8s, err = internal.NewKubernetes(cfg)
+	}
 	if err != nil {
 		log.Error(err)
 		os.Exit(1)
 	}
+
+	crds, err := internal.NewCRDMap(cfg, k8s)
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+
 	if len(crds) <= 1 {
 		log.Warn("No CRDs were loaded ... no validation will be done")
 	}
-	if len(crds) > 0 {
-		log.Info("Custom Resource Definitions (CRDs)")
-		for key := range crds {
-			log.Info("  ", key)
-		}
-	}
 
 	log.Trace("main() ->")
-}
-
-func loadCRDs(cfg internal.Config) (internal.CRDMap, error) {
-	if cfg.Offline() {
-		return loadCRDsFromPath(cfg)
-	}
-	return loadCRDsFromKubernetes(cfg)
-}
-
-func loadCRDsFromKubernetes(cfg internal.Config) (internal.CRDMap, error) {
-	k8s, err := internal.NewKubernetes(cfg)
-	if err != nil {
-		return nil, err
-	}
-	return k8s.CRDMap, nil
-}
-
-func loadCRDsFromPath(cfg internal.Config) (internal.CRDMap, error) {
-	return make(internal.CRDMap), nil
 }
